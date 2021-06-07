64afa1d6761498ea88091d5db3e0966cd00f5e94
LadCoder <hbk.lekkas@gmail.com>
2021-06-07 20:50:49 +1000

    Fix node_modules path

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ echo "* Setting up file permissions *"
 sudo chown -R $USER:$USER .
 
 echo "* Remove any node dependencies *"
-rm -r $HOME/node_modules
+rm -r node_modules
 
 echo "* Installing NPM packages *"
 npm install
